4dfaada48a1e43361587785651ce55c432c2f611
Joe Hamman <jhamman@hydro.washington.edu>
2013-02-04 23:15:54 -0800

    Still working on netcdf2vic.py(cfg)
    Further improvements to command line / configuration call.
    Added configuration file (template)

--- tools/netcdf2vic.py
+++ tools/netcdf2vic.py
@@ -4,15 +4,12 @@ import numpy as np
 import struct
 from netCDF4 import Dataset
 import time as tm
-import sys
+import sys, os
 import datetime
+import ConfigParser
 
-def main():
-    outFile = 'data_'+??
-    precipFile
-    tempFile
-    windFile
-    outPrefix = 'data_'
+def main(Files,Points,Full_Grid,output,Binary_Mult,Binary_Type,Paths,outPrefix,verbose):
+    if Full_Grid:
 
 def process_command_line():
     """
@@ -28,22 +25,26 @@ def process_command_line():
     parser.add_argument("-W","--windFile", type=str, help="Input netCDF containing wind data")
     parser.add_argument("-LON","--longitude", type=float, help="Input longitude of point to access data")
     parser.add_argument("-LAT","--latitude", type=float, help="Input latitude of point to access data")
-    parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
-    parser.add_argument("-C", "--config", help="Use Configuration File", action="store_true")
+    parser.add_argument("-v","--verbose", help="Increase output verbosity", default = False,action="store_true")
+    parser.add_argument("-C","--config", help="Use Configuration File", action="store_true")
+    parser.add_argument("-B","--Binary", help="output in binary format (default)",action="store_true")
+    parser.add_argument("-T","--ASCII", help="output in ASCII format",action="store_true")
     args = parser.parse_args()
     if args.config:
         print 'Using configuration file'
+        (Files,Points,Full_Grid,output,Binary_Mult,Binary_Type,Paths,verbose) = process_config(configFile)
     else:
-        if args.allfile: precipFile = tmaxFile = tminFile = windFile = args.allFile
-        if args.precipFile: precipFile = args.precipFile
-        if args.tmaxFile: tmaxFile = args.tmaxFile
-        if args.tminFile: tminFile = args.tminFile
-        if args.windFile: windFile = args.windFile
+        verbose = args.verbose
+        if args.allfile: 
+            Files = {'precip':[args.allFile],'tmax':[args.allFile],'tmin':[args.allFile],'wind':[args.allFile]}
+        if args.precipFile: files['precip'] = [args.precipFile]
+        if args.tmaxFile: files['tmax'] = [args.tmaxFile]
+        if args.tminFile: files['tmin'] = [args.tminFile]
+        if args.windFile: files['wind'] = [args.windFile]
         if (args.longitude and args.latitude):
-            lon = args.longitude
-            lat = args.latitude
+            Points = [(args.longitude,args.latitude)]
             Full_Grid = False
-            print 'Processing data from grid cell at lon/lat:',str(lon),',',str(lat)
+            if verbose: print 'Processing data from grid cell at lon/lat:',str(lon),',',str(lat)
         elif args.longitude:
             raise NameError("Need both -LON & -LAT args if one is specified, if neither are specified, the full grid will be processed into individual files")
         elif args.latitude:
@@ -51,17 +52,46 @@ def process_command_line():
         else:
             print "Processing all grid cells in input data set(s)"
             Full_Grid = True
-    print 'Reading Precip data from:',precipFile
-    print 'Reading Tmax data from:',tmaxFile
-    print 'Reading Tmin data from:',tminFile
-    print 'Reading Wind data from:',windFile
-    
-    
-def process_config():
+            Points = [()]
+        if (args.Binary==False and args.ASCII==False):
+            Output = {'Binary':True,'ASCII':False}
+            if verbose: print 'No output data type was specified (-B or -T) so default will be used (Binary).'
+        else: Output = {'Binary':args.Binary,'ASCII':args.ASCII}
+        outPrefix = 'data_'
+        Paths = {'inPath': os.getcwd, 'outPath':os.getcwd()}
+        Binary_Mult = {'precip':40,'tmax':100,'tmin':100,'wind':100}
+        Binary_Type = {'precip':np.uint16,'tmax':np.int16,'tmin':np.int16,'wind':np.int16}
+    if verbose: print 'Reading Precip data from:',files
+    return (Files,Points,Full_Grid,output,Binary_Mult,Binary_Type,Paths,outPrefix,verbose)
+        
+def process_config(configFile):
     """
-    Parse arguments and assign flags for further loading of variables, for
-    information on input arguments, type rout.py -h
+    Parse arguments and assign flags for further loading of files.  Configuration
+    flag must be raised on command line (-C) to configuration file must be provided.
+    Usage:  netcdf2vic.py -C netcdf2vic.cfg
     """
+    config = ConfigParser.ConfigParser()
+    config.read(configFile)
+    config.sections()
+    keys = config.get('Basics','Var_Keys').split(',')
+    verbose = config.getboolean('Basics','verbose')
+    Full_Grid = config.getboolean('Basics','Full_Grid')
+    output = {'Binary':config.getboolean('Basics','Binary'),
+              'ASCII' = config.getboolean('Basics','ASCII')}
+    Paths = {'inPath':config.get('Paths','inPath'),
+             'outPath': config.get('Paths','outPath')}
+    Binary_Mult = {}
+    Binary_Type = {}
+    Files = {}
+    for key in keys:
+        Binary_Mult[key] = config.getint(key,'Binary_Mult')
+        Binary_Type[key] = config.get(key,'Binary_Type')
+        Files[key] = config.get(key,'Files').split(',') 
+    if Full_Grid == True:
+        Points = [()]
+    else:
+        Points = zip(config.get('Points','Lons'),config.get('Points','Lats'))
+    return (Files,Points,Full_Grid,output,Binary_Mult,Binary_Type,Paths,outPrefix,verbose)
 
 ##################################################################################
 ## Read netCDF Inputs

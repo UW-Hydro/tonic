cb3a306a6e36b3be68e50b43a31410c62ed0f382
Joe Hamman <jhamman@hydro.washington.edu>
2015-07-08 09:30:41 -0700

    add docstrings to functions

--- tonic/models/flo2d/utils.py
+++ tonic/models/flo2d/utils.py
@@ -8,7 +8,7 @@ from scipy import stats
 import quantities as pq
 from scipy.spatial import cKDTree
 
-
+# Default projection from Alan Hamlet 2015-06-29
 default_proj = pyproj.Proj('+proj=lcc +lat_1=47.5 +lat_2=48.73333333333333 '
                            '+lat_0=47'' +lon_0=-120.8333333333333 '
                            '+x_0=609601.2192024385 +y_0=0 +datum=NAD83 '
@@ -16,13 +16,27 @@ default_proj = pyproj.Proj('+proj=lcc +lat_1=47.5 +lat_2=48.73333333333333 '
 
 
 def esriprj2standards(shapeprj_path, kind='proj4'):
+    '''Return string representation of projection in shapeprj_path.
+
+    Parameters
+    ----------
+    shapeprj_path : str
+        Filepath to ESRI style projection file.
+    kind : str {'prj', 'wkt', 'proj4', 'epsg'}
+        Projection standard.
+
+    Returns
+    ----------
+    projection_params : str
+        Projection parameters.
+    '''
     prj_file = open(shapeprj_path, 'r')
     prj_txt = prj_file.read()
     srs = osr.SpatialReference()
     srs.ImportFromESRI([prj_txt])
     if kind.lower() == 'prj':
         return prj_txt
-    if kind.lower() == 'wtk':
+    if kind.lower() == 'wkt':
         return srs.ExportToWkt()
     elif kind.lower() == 'proj4':
         return srs.ExportToProj4()
@@ -33,13 +47,28 @@ def esriprj2standards(shapeprj_path, kind='proj4'):
         raise ValueError('Kind is unknown')
 
 
-def read_flo2d_depth_file(fname):
+def read_flo2d_depth_file(fname, names=('x', 'y', 'depth')):
+    '''Parser helper function to read ASCII output from FLO-2D
+
+    Parameters
+    ----------
+    fname : str
+        Filepath to FLO-2D depth file.
+    names : tuple of str, optional
+        Header names
+
+    Returns
+    ----------
+    df : Pandas.DataFrame
+        DataFrame of FLO-2D output.
+    '''
     return pd.read_table(fname, sep='\s+',
-                         names=['x', 'y', 'depth'],
+                         names=names,
                          index_col=0)
 
 
 def make_coordinates(arr):
+    '''Calculate 1D coordinate vector from array of coordinates'''
     min_arr = arr.min()
     max_arr = arr.max()
     d_arr = stats.mode(np.diff(sorted(arr.unique())))[0][0]
@@ -47,6 +76,22 @@ def make_coordinates(arr):
 
 
 def grid_flo2d_depth(fname):
+    '''Read and grid flow2d depth file output
+
+    Parameters
+    ----------
+    fname : str
+        Filepath to FLO-2D depth file.
+
+    Returns
+    ----------
+    data: 2d numpy array
+        Gridded depth data
+    gys: 2d numpy array
+        Gridded y coordinates
+    gxs: 2d numpy array
+        Gridded x coordinates
+    '''
     df = read_flo2d_depth_file(fname)
 
     xs = make_coordinates(df.x)
@@ -58,7 +103,7 @@ def grid_flo2d_depth(fname):
                              np.array(df.x.values))).transpose())
 
     mytree = cKDTree(combined)
-    dist, indexes = mytree.query(points, k=1)
+    _, indexes = mytree.query(points, k=1)
     y, x = np.unravel_index(indexes, gxs.shape)
 
     data = np.zeros_like(gys)
@@ -68,6 +113,24 @@ def grid_flo2d_depth(fname):
 
 
 def flo2d_coords_to_geographic(x, y, proj=default_proj):
+    '''Convert local projection coordinates to geographic (lat/lon) coordinates
+
+    Parameters
+    ----------
+    x : array_like
+        x coordinates, units=ft
+    y : array_like
+        y coordinates, units=ft
+    proj : pyproj.Proj
+        Projection object
+
+    Returns
+    ----------
+    lons: array_like
+        longitude values corresponding to x
+    lats: array_like
+        latitude values corresponding to y
+    '''
     x_ft = x * pq.ft
     x_m = x_ft.rescale(pq.m)
 

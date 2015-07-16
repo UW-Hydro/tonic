173a5faaa78c05a8cc95219a1c03d9b7f4a9a878
Joe Hamman <jhamman@hydro.washington.edu>
2015-07-16 10:53:02 -0700

    fix failing unit test

--- tests/test_vic2netcdf.py
+++ tests/test_vic2netcdf.py
@@ -6,25 +6,12 @@ Set to run with pytest
 
 Usage: py.test (from RVIC or test directory)
 """
-import pytest
 import numpy as np
-import sys
-sys.path.append("../")
+from tonic.tonic import calc_grid
 
-# -------------------------------------------------------------------- #
-# Unit tests for vic2netcdf.py
-
-
-@pytest.fixture
-def lons():
-	pass
 
 def test_calc_grid_standard():
-    from processing_tools.vic2netcdf import calc_grid
-    lons = np.arange(0, 5, 0.5)
+    lons = np.arange(0, 5, 0.25)
     lats = np.arange(0, 10, 0.5)
     target_grid = calc_grid(lons, lats, decimals=4)
-
-# -------------------------------------------------------------------- #
-
-
+    assert type(target_grid) == dict

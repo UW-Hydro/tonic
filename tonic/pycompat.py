5f7b7fa68a6d33aeded33ba4b4e8ac7623595d9a
Joe Hamman <jhamman1@uw.edu>
2015-07-17 16:52:42 -0700

    add pycompat module and included some minro python3 fixes

---
+++ tonic/pycompat.py
@@ -0,0 +1,45 @@
+import sys
+
+PY3 = sys.version_info[0] >= 3
+
+if PY3:  # pragma: no cover
+    basestring = str
+    unicode_type = str
+    bytes_type = bytes
+
+    def iteritems(d):
+        return iter(d.items())
+
+    def itervalues(d):
+        return iter(d.values())
+
+    pyrange = range
+    pyzip = zip
+    from functools import reduce
+    import builtins
+    from configparser import SafeConfigParser
+else:  # pragma: no cover
+    # Python 2
+    basestring = basestring
+    unicode_type = unicode
+    bytes_type = str
+
+    def iteritems(d):
+        return d.iteritems()
+
+    def itervalues(d):
+        return d.itervalues()
+
+    pyrange = xrange
+    from itertools import izip as pyzip
+    from intertoos import imap as pymap
+    pyreduce = reduce
+    import __builtin__ as builtins
+    from ConfigParser import SafeConfigParser
+try:
+    from cyordereddict import OrderedDict
+except ImportError:  # pragma: no cover
+    try:
+        from collections import OrderedDict
+    except ImportError:
+        from ordereddict import OrderedDict
